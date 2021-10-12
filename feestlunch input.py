528b19e66dd798c5e3c60e59a5ab725f2cbcb948
mateo1665 <89844584+mateo1665@users.noreply.github.com>
2021-10-12 11:25:32 +0200

    Add files via upload

---
+++ feestlunch input.py
@@ -0,0 +1,41 @@
+print ('croisantjes')
+croisantjes = input ('hoeveel croisantjes')
+print (croisantjes)
+
+print ('prijs van een croisant')
+croisant = input (' een croisant is 0.39')
+print (croisant)
+
+print ('stokbrooden')
+stokbrooden = input ('hoeveel stokbrooden')
+print (stokbrooden)
+
+print ('prijs van een stokbrood')
+stokbrood = input ('een stokbrood is 2.78')
+print (stokbrood)
+
+print ('kortingsbonnen')
+kortingsbonnen = input ('hoeveel kortingsbonnen')
+print (kortingsbonnen)
+
+print ('prijs van een krotingsbon')
+kortingsbon = input ('een kortingsbon is 0.50')
+print (kortingsbon)
+
+print ('prijs van kortingsbon gaat eraf')
+kortingsbon =input (' prijs van een kortingsbon wat eraf gaat is 0.50')
+print (kortingsbon)
+
+input (croisantjes * croisant)
+total = input (croisantjes * croisant)
+
+input (stokbrooden * stokbrood)
+total = input (stokbrooden * stokbrood)
+
+input (kortingsbonnen * kortingsbon)
+total = input (kortingsbonnen * kortingsbon)
+
+input  (croisantjes + stokbrooden - kortingsbonnen)
+total = input (croisantjes + stokbrooden - kortingsbonnen)
+
+total = input 
---
+++ speeldaghal input.py
@@ -0,0 +1,28 @@
+from speelhaldag import VRminuten
+
+print ('personen')
+personen = input ('hoeveel personen')
+print (personen)
+
+print ('prijs per persoon')
+persoon = input ('prijs per persoon is 7.45')
+print (persoon)
+
+print ('VR minuten')
+VRminuten = input (' met de VR heb je 5 minuten')
+print (VRminuten)
+
+print ('prijs van de VR minuten')
+prijsVRminuten = input (' prijs VR per 5 minuten is 0.37')
+print (prijsVRminuten)
+
+input (personen * persoon)
+total = input (personen * persoon)
+
+print (VRminuten * prijsVRminuten)
+total = input (VRminuten * prijsVRminuten)
+
+print (prijsVRminuten + personen)
+total = input (prijsVRminuten + personen)
+
+total = input
